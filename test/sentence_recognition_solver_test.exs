0c784508fc3813d7d5a2f6632c5e03da7bad715a
Lucas de Menezes <lucasmenezes098@gmail.com>
2022-03-10 23:25:43 -0300

    added unnecessary comments to test file

--- test/sentence_recognition_solver_test.exs
+++ test/sentence_recognition_solver_test.exs
@@ -2,6 +2,7 @@ defmodule SentenceRecognitionSolverTest do
 	use ExUnit.Case
 	doctest SentenceRecognitionSolver
 
+	# Gramática G = ({S, A, B}, {a, b}, {S -> AA, S -> B, A -> Ab, AA -> AA, B -> Bb, B -> b, Abb -> BBB}, S)
 	test "Teste 1" do
 		grammar = {[{"S", "AA"}, {"S", "B"}, {"A", "Ab"}, {"AA", "aa"}, {"B", "Bb"}, {"B", "b"}, {"Abb", "BBB"}], "S"}
 		assert SentenceRecognitionSolver.isSentenceOnGrammar(grammar, "aabb")
@@ -10,12 +11,14 @@ defmodule SentenceRecognitionSolverTest do
 		assert not SentenceRecognitionSolver.isSentenceOnGrammar(grammar, "aaa")
 	end
 
+	# Gramática G = ({S, A}, {a}, {S -> A, A -> a}, S)
 	test "Teste 2" do
 		grammar = {[{"S", "A"}, {"A", "a"}], "S"}
 		assert SentenceRecognitionSolver.isSentenceOnGrammar(grammar, "a")
 		assert not SentenceRecognitionSolver.isSentenceOnGrammar(grammar, "aa")
 	end
 
+	# Gramática G = ({S, A}, {ab}, {S -> aAS, S -> a, A -> SbA, A -> ba, A -> SS}, S)
 	test "Teste 3" do
 		grammar = {[{"S", "aAS"}, {"S", "a"}, {"A", "SbA"}, {"A", "ba"}, {"A", "SS"}], "S"}
 		assert SentenceRecognitionSolver.isSentenceOnGrammar(grammar, "abaa")
